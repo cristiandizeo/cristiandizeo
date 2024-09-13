62d91dcae77f7668df48f88545f1076f2f9a314e
Cristian <dizeoc@gmail.com>
2024-09-13 11:22:55 -0300

    update js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let msjEnviado = document.getElementById("msjEnviado");
-  let msjNoEnviado = document.getElementById("msjNoEnviado");
-  let submit = document.getElementById("submit");
   let btnInicio = document.getElementById("inicio");
 
   let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
@@ -19,56 +16,4 @@ document.addEventListener("DOMContentLoaded", function () {
       btnInicio.style.display = "none";
     }
   }
-
-  // FORMULARIO
-  // definimos el formulario a usar formFile
-  const formFile = document.getElementById('form');
-
-  submit.addEventListener('click', function (event) {
-    // definimos la accion clic al pulsar el boton submit
-    if (formFile.checkValidity()) {
-
-      event.preventDefault(); // anulamos que boton nos lleve a otro lado
-
-      //usamos FormData para compilar los datos a enviar
-      const formattedFormData = new FormData(formFile);
-      // llamamos a una funcion que enviara los datos,
-      // como parametro pasamos los datos del formulario
-      postData(formattedFormData);
-
-      submit.disabled = true;
-      msjEnviado.style.display = "none";
-      msjNoEnviado.style.display = "none";
-      document.getElementById("alertaEnviando").style.display = "block";
-
-    }
-    
-      // nuestra función personalizada que envia datos y recibe respuesta del servidor
-      // usamos async/await para trabajar de mejor manera la respuesta por parte del servidor
-    
-      // en fetch especificamos el archivo en el servidor que captura los datos enviados
-      async function postData(formattedFormData) {
-        const response = await fetch('email.php', {
-          // el metodo a usar
-          method: 'POST',
-          // los datos a ser enviados
-          body: formattedFormData
-        });
-    
-        document.getElementById("alertaEnviando").style.display = "none";
-        // data contendra la respuesta del servidor
-        const data = await response.text();
-    
-        if (data == 1) {
-          document.getElementById("form").reset();
-          msjEnviado.style.display = "block";
-        } else {
-          msjNoEnviado.style.display = "block";
-        }
-        submit.disabled = false;
-      }
-
-
   });
-
-});
