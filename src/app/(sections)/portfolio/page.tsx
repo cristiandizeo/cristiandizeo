00ff184907765f0c1200de7fb224ff636b3e7de7
Cristian Dizeo <dizeoc@gmail.com>
2025-02-17 14:21:19 -0300

    upd portfolio

--- src/app/(sections)/portfolio/page.tsx
+++ src/app/(sections)/portfolio/page.tsx
@@ -3,32 +3,39 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { X, FileText } from "lucide-react";
+import { X, FileText, XCircle } from "lucide-react";
 import { SiTypescript } from "react-icons/si";
 import { FaFolder, FaLaravel, FaPhp, FaReact } from "react-icons/fa";
-import { DiPhp } from "react-icons/di";
+import Image from "next/image";
 
 const projects = [
   {
     name: "Vocaltech.tsx",
-    description: "Plataforma innovadora que combina la voz y la tecnología para potenciar marcas y desarrollar talento.",
+    description:
+      "Plataforma innovadora que combina la voz y la tecnología para potenciar marcas y desarrollar talento.",
     tech: ["React", "TypeScript", "Zustand"],
     codeUrl: "https://github.com/No-Country-simulation/equipo-h4-01-vocaltech",
     liveUrl: "https://equipo-h4-01-vocaltech.vercel.app/",
+    imageUrl: "/vocaltech.webp",
   },
   {
     name: "NotepadAI.jsx",
-    description: "Clon ligero de NotebookLM con interacción avanzada de documentos.",
+    description:
+      "Clon ligero de NotebookLM con interacción avanzada de documentos.",
     tech: ["NextJS", "TypeScript", "C#", "Gemini AI"],
     codeUrl: "https://github.com/No-Country-simulation/s19-10-ft-webapp",
     liveUrl: "#",
+    imageUrl: "/notepadai.webp",
   },
   {
     name: "Petgram.blade.php",
-    description: "Una app para encontrar mascotas perdidas, con autenticación y mapas en tiempo real.",
+    description:
+      "Una app para encontrar mascotas perdidas, con autenticación y mapas en tiempo real.",
     tech: ["React", "TailwindCSS", "Laravel", "Cloudinary"],
-    codeUrl: "https://github.com/No-Country-simulation/grupo-c21-37-n-php-react",
+    codeUrl:
+      "https://github.com/No-Country-simulation/grupo-c21-37-n-php-react",
     liveUrl: "#",
+    imageUrl: "/petgram.webp",
   },
   {
     name: "GrasmanAutos.php",
@@ -36,19 +43,29 @@ const projects = [
     tech: ["PHP", "CSS3", "HTML5", "Wordpress"],
     codeUrl: "#",
     liveUrl: "https://www.grasmanautos.com/",
+    imageUrl: "/grasmanautos.webp",
   },
 ];
 
 const getFileIcon = (name: string) => {
-  if (name.endsWith(".tsx")) return <SiTypescript size={16} className="text-blue-400" />;
-  if (name.endsWith(".jsx")) return <FaReact size={16} className="text-blue-400" />;
-  if (name.endsWith(".blade.php")) return <FaLaravel size={16} className="text-red-600" />;
-  if (name.endsWith(".php")) return <FaPhp size={24} className="text-blue-400" />;
+  if (name.endsWith(".tsx"))
+    return <SiTypescript size={16} className="text-blue-400" />;
+  if (name.endsWith(".jsx"))
+    return <FaReact size={16} className="text-blue-400" />;
+  if (name.endsWith(".blade.php"))
+    return <FaLaravel size={16} className="text-red-600" />;
+  if (name.endsWith(".php"))
+    return <FaPhp size={24} className="text-blue-400" />;
   return <FileText size={16} className="text-gray-400" />;
 };
 
 export default function Portfolio() {
   const [openProjects, setOpenProjects] = useState<string[]>([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para controlar el menú
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Cambia el estado al hacer clic
+  };
 
   const toggleProject = (name: string) => {
     setOpenProjects((prev) =>
@@ -57,51 +74,50 @@ export default function Portfolio() {
   };
 
   return (
-    <div className="h-screen bg-gray-950 text-gray-200 p-10 overflow-auto">
-      {/* Sidebar con archivos */}
-      <aside className="fixed top-[3.3rem] left-0.5 h-full w-1/6 bg-[#252526] border-r border-gray-700 p-4">
-        <h3 className="flex gap-2 items-center text-gray-400 text-sm mb-4"><FaFolder size={16} /> Proyectos</h3>
-        <ul className="space-y-2 ml-4">
-          {projects.map((project) => (
-            <motion.li
-              key={project.name}
-              onClick={() => toggleProject(project.name)}
-              className="flex items-center space-x-2 cursor-pointer text-gray-300 hover:font-bold"
-            >
-              {getFileIcon(project.name)}
-              <span>{project.name}</span>
-            </motion.li>
-          ))}
-        </ul>
-      </aside>
+    <div className="flex flex-col md:flex-row h-screen bg-gray-950 text-gray-200 overflow-auto">
+    {/* Sidebar con archivos */}
+    <aside className="w-full md:w-1/3 lg:w-1/4 md:h-full bg-[#252526] border-r border-gray-700 p-4">
+      {/* Botón para abrir/cerrar el menú */}
+      <button
+        onClick={toggleMenu}
+        className="flex gap-2 items-center text-gray-400 text-sm w-full hover:text-gray-200 focus:outline-none"
+      >
+        <FaFolder size={16} /> Proyectos
+      </button>
 
-      {/* Contenido */}
-      <div className="ml-52">
-        {/* Pestañas abiertas */}
-        <div className="flex space-x-1 border-b border-gray-700 p-2">
-          {openProjects.map((name) => (
-            <motion.div
-              key={name}
-              initial={{ opacity: 0, x: -10 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 10 }}
-              className="flex items-center bg-[#2d2d2d] text-gray-300 px-3 py-2 rounded-t-md"
-            >
-              {getFileIcon(name)}
-              <span className="ml-2">{name}</span>
-              <button
-                onClick={() => toggleProject(name)}
-                className="ml-2 opacity-50 hover:opacity-100"
-              >
-                <X size={14} />
-              </button>
-            </motion.div>
-          ))}
+      {/* Menú desplegable */}
+      <div
+          className={`overflow-hidden transition-all duration-500 ease-in-out ${
+            isMenuOpen ? "max-h-96" : "max-h-0"
+          } md:max-h-full`}
+        >
+      <ul className={`space-y-2 m-2`}>
+        {projects.map((project) => (
+          <motion.li
+            key={project.name}
+            onClick={() => toggleProject(project.name)}
+            className="flex items-center space-x-2 cursor-pointer text-gray-300 hover:font-bold"
+          >
+            {getFileIcon(project.name)}
+            <span>{project.name}</span>
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+    </aside>
+
+    {/* Contenido */}
+    <div className="w-full md:w-5/6 h-full">
+      {openProjects.length === 0 && (
+        <div className="flex p-4 justify-center items-center min-h-[200px]">
+          <p className="text-gray-400 text-center text-lg md:text-3xl">
+            ¡Hola! Podés ver mis proyectos haciendo click en ellos
+          </p>
         </div>
-
-        {/* Detalles de los proyectos */}
-        <div className="m-4">
-          {openProjects.map((name) => {
+      )}
+      <div className="grid items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {openProjects.length > 0 &&
+          openProjects.map((name) => {
             const project = projects.find((p) => p.name === name);
             return project ? (
               <motion.div
@@ -110,30 +126,62 @@ export default function Portfolio() {
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 10 }}
                 transition={{ duration: 0.3 }}
-                className="p-5 m-2 bg-[#252526] border border-gray-700 rounded-lg space-y-4"
+                className="h-[28rem] bg-[#252526] border border-gray-700 rounded-lg overflow-hidden shadow-lg"
               >
-                <h2 className="text-xl font-bold text-teal-400">{project.name}</h2>
-                <p className="text-gray-300">{project.description}</p>
-                <div className="flex space-x-2">
-                  {project.tech.map((tech) => (
-                    <span key={tech} className="bg-gray-700 px-2 py-1 text-xs rounded">
-                      {tech}
-                    </span>
-                  ))}
+                {/* Imagen del Proyecto */}
+                <div className="relative w-full h-48">
+                  <Image
+                    src={project.imageUrl}
+                    alt={project.name}
+                    fill
+                    className="object-cover grayscale hover:grayscale-0 transition-all duration-300"
+                  />
+                  <button
+                    onClick={() => toggleProject(name)}
+                    aria-label="Cerrar proyecto"
+                    className="absolute top-2 right-2 bg-black/50 p-1 rounded-full hover:bg-black transition-all"
+                  >
+                    <XCircle size={24} className="text-white" />
+                  </button>
                 </div>
-                <div className="flex space-x-4 mt-3">
-                  <Link target="_blank" href={project.codeUrl} className="text-sm text-teal-400 hover:underline">
-                    📜 Ver Código
-                  </Link>
-                  <Link target="_blank" href={project.liveUrl} className="text-sm text-teal-400 hover:underline">
-                    🔗 Ver Demo
-                  </Link>
+
+                {/* Contenido del Proyecto */}
+                <div className="p-5 space-y-3">
+                  <h2 className="text-xl font-bold text-teal-400">{project.name}</h2>
+                  <p className="text-gray-300">{project.description}</p>
+
+                  {/* Tecnologías */}
+                  <div className="flex flex-wrap gap-2">
+                    {project.tech.map((tech) => (
+                      <span key={tech} className="bg-gray-700 px-2 py-1 text-xs rounded">
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
+
+                  {/* Links */}
+                  <div className="flex justify-between mt-3">
+                    <Link
+                      target="_blank"
+                      href={project.codeUrl}
+                      className="text-sm text-teal-400 hover:underline"
+                    >
+                      📜 Ver Código
+                    </Link>
+                    <Link
+                      target="_blank"
+                      href={project.liveUrl}
+                      className="text-sm text-teal-400 hover:underline"
+                    >
+                      🔗 Ver Demo
+                    </Link>
+                  </div>
                 </div>
               </motion.div>
             ) : null;
           })}
-        </div>
       </div>
     </div>
+  </div>
   );
 }
